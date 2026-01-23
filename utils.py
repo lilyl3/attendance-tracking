021e469f8e2453c901c2cd9888caee5f3f5e7910
lilyl3 <lily.lin.915@gmail.com>
2026-01-22 23:26:08 -0800

    Add arg to determine how date is formatted

--- utils.py
+++ utils.py
@@ -1,15 +1,22 @@
 from datetime import date, timedelta
 
-def today():
-    return date.today().isoformat();
+# Iso formats date as YYYY-MM-DD
+def format_date(date, iso):
+    if iso:
+        return date.today().isoformat()
+    else:
+        return date.today()
+    
+def today(iso = True):
+    return format_date(date.today(), iso)
 
-# Return the most recent Sunday
-def most_recent_sunday():
+def most_recent_sunday(iso = True):
+    # Return the most recent Sunday
     today = date.today()
     most_recent_sunday = today - timedelta(days=(today.weekday() + 1) % 7) # sunday = 6
-    return most_recent_sunday.isoformat()
+    return format_date(most_recent_sunday, iso)
 
-def get_date_months_ago(delta_month = 3):
+def get_date_months_ago(delta_month = 3, iso = True):
     today = date.today()
     date_months_ago = today - timedelta(months=delta_month)
-    return date_months_ago.isoformat()
+    return format_date(date_months_ago, iso)
