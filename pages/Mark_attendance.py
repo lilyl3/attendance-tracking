99e6eef5643f881e33fa02e4b4d199ada22047ba
lilyl3 <lily.lin.915@gmail.com>
2026-01-23 23:46:38 -0800

    Only rerender page if navigate away

--- pages/Mark_attendance.py
+++ pages/Mark_attendance.py
@@ -4,3 +4,6 @@ from config_pages import set_page, display_organization, display_date
 set_page()
 display_organization()
 display_date()
+
+if "page" not in st.session_state or st.session_state["page"] != "mark_attendance":
+    st.session_state["page"] = "mark_attendance"
