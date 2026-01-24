379c76561e31fafbbf4659fba99a7acf7a5fb015
lilyl3 <lily.lin.915@gmail.com>
2026-01-23 23:41:36 -0800

    Create database object once per session via cache

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import streamlit as st
-from config_pages import set_page
+from config_pages import set_page, display_organization
+from db.database import AttendanceDB
+
+# Connect to database
+@st.cache_resource
+def get_db():
+    return AttendanceDB("attendance.db")
 
 set_page()
 
 pg = st.navigation(
-    ["pages/Dashboard.py", "pages/Mark_attendance.py"],
+    ["pages/Dashboard.py", "pages/Mark_attendance.py", "pages/Add_Member.py"],
     position = "top"
 )
 pg.run()
