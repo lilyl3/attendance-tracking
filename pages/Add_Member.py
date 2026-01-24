872db0c210f57f07978fc82459f5291bc80ed047
lilyl3 <lily.lin.915@gmail.com>
2026-01-24 13:27:17 -0800

    Remove comments about st.session_state for rerendering

--- pages/Add_Member.py
+++ pages/Add_Member.py
@@ -4,12 +4,11 @@ from app import get_db
 
 db = get_db()
 
-# if "page" not in st.session_state or st.session_state["page"] != "add_member_page":
 set_page()
 display_organization()
 st.subheader("Add Member", divider="red")
 
-with st.form(key = "add_member_form", clear_on_submit=True):
+with st.form(key="add_member_form", clear_on_submit=True):
     english_name = st.text_input("English Name 英文名")
     chinese_name = st.text_input("Chinese Name 中文名")
     submitted = st.form_submit_button("Enter")
@@ -17,5 +16,3 @@ with st.form(key = "add_member_form", clear_on_submit=True):
 if submitted:
     db.add_member(english_name, chinese_name)
     st.success("Member added! 已加成员!")
-
-    # st.session_state["page"] = "add_member_page"
