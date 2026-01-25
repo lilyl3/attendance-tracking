29e970b61c718aab5d1dba37e88606c6d77c33c6
lilyl3 <lily.lin.915@gmail.com>
2026-01-25 00:14:41 -0800

    Change name of dir storing csv of sunday attendance

--- pages/Mark_attendance.py
+++ pages/Mark_attendance.py
@@ -7,7 +7,7 @@ from config_pages import set_page, display_organization, display_date
 from app import get_db
 from utils import most_recent_sunday
 
-file_name = "attendance/" + most_recent_sunday() + ".csv"
+file_name = "sunday_attendance/" + most_recent_sunday() + ".csv"
     
 set_page()
 display_organization()
@@ -17,22 +17,22 @@ db = get_db()
 data = db.get_members()
 data_df = pd.DataFrame(
     data,
-    columns=["MemberID", "English Name", "Chinese Name", "Present?"]
+    columns=["MemberID", "English Name 英文名", "Chinese Name 中文名", "Present 在场吗?"]
 )
-# Present? stores date formatted as YYYY-MM-DD => If str exists, then previously marked member as attended
-data_df["Present?"] = data_df["Present?"].apply(lambda x: bool(x) if pd.notna(x) else False).astype("boolean")
+# Present 在场吗? stores date formatted as YYYY-MM-DD => If str exists, then previously marked member as attended
+data_df["Present 在场吗?"] = data_df["Present 在场吗?"].apply(lambda x: bool(x) if pd.notna(x) else False).astype("boolean")
 data_df = data_df.set_index("MemberID")
-data_df = data_df.sort_values(by="English Name")
+data_df = data_df.sort_values(by="English Name 英文名")
 
 edited_df = st.data_editor(
     data_df,
     column_config={
-        "Present?": st.column_config.CheckboxColumn(
-            "Present?",
+        "Present 在场吗?": st.column_config.CheckboxColumn(
+            "Present 在场吗?",
             default=False,
         )
     },
-    disabled=["English Name", "Chinese Name"],
+    disabled=["English Name 英文名", "Chinese Name 中文名"],
     hide_index=True,
 )
 
@@ -41,13 +41,13 @@ edited_df.to_csv(file_name)
 # print("****")
 # print(data_df)
 # print(edited_df)
-mask = edited_df["Present?"] != data_df["Present?"]
+mask = edited_df["Present 在场吗?"] != data_df["Present 在场吗?"]
 if (mask.any()):
     member_id = int(edited_df[mask].index[0])
-    if edited_df.loc[member_id, "Present?"]:
-        # print("Marked", edited_df.loc[member_id, "English Name"], "as present")
+    if edited_df.loc[member_id, "Present 在场吗?"]:
+        # print("Marked", edited_df.loc[member_id, "English Name 英文名"], "as present")
         db.add_attendance(member_id)
     else:
-        # print("Marked", edited_df.loc[member_id, "English Name"], "as absent")
+        # print("Marked", edited_df.loc[member_id, "English Name 英文名"], "as absent")
         db.delete_attendance(member_id)
 # print("****")
