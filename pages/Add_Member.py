a5d76aca7c4462784f52dc2e8ee8901c58b22bee
lilyl3 <lily.lin.915@gmail.com>
2026-02-28 12:39:05 -0800

    Add in more fields for members, store by family, auto add attendance

--- pages/Add_Member.py
+++ pages/Add_Member.py
@@ -1,21 +1,91 @@
 import streamlit as st
+import time
+
 from config_pages import set_page, display_organization, get_db
+from utils import most_recent_sunday
+
+def reset_all():
+    st.session_state["family"] = []
+    st.session_state["family_name"] = ""
+    st.session_state["family_id"] = ""
+    st.session_state["invited_by"] = ""
 
 def Add_Member():
     db = get_db()
 
     set_page()
     display_organization()
-    st.subheader("Add Member", divider="red")
 
     if "page" not in st.session_state or st.session_state["page"] != "add_member":
         st.session_state["page"] = "add_member"
+        st.session_state["family"] = []
+        st.session_state["family_name"] = ""
+        st.session_state["family_id"] = ""
+        st.session_state["invited_by"] = ""
+        
+    st.header("Register 登记", divider="red")
+    # -------------------------
+    # No family name recorded
+    # -------------------------
+    st.text_input("Family Name 姓氏", key = "family_name")
+    st.text_input("Invited by 邀请人", key="invited_by")
 
-    with st.form(key="add_member_form", clear_on_submit=True):
-        english_name = st.text_input("English Name 英文名")
-        chinese_name = st.text_input("Chinese Name 中文名")
-        submitted = st.form_submit_button("Enter")
+    if not st.session_state["family_id"]:
+        if st.button("Save 存家庭信息") and st.session_state["family_name"]:
+            # Add family to database
+            family_id = db.add_family(
+                st.session_state["family_name"], 
+                most_recent_sunday(), 
+                st.session_state["invited_by"]
+            )
+            st.session_state["family_id"] = family_id
+            st.rerun()
 
-    if submitted:
-        db.add_member(english_name, chinese_name)
-        st.success("Member added! 已加成员!")
+    if st.session_state["family_id"]:
+        # -------------------------
+        # Show added members
+        # -------------------------
+        st.write("Members 成员")
+        for i, m in enumerate(st.session_state["family"]):
+            st.write(f"{i+1}. {m[0]} {m[1]}")
+
+        st.button("Reset 重置", on_click=reset_all)
+
+        st.subheader("Add Member 加成员", divider="red")
+        with st.form(key="add_member_form", clear_on_submit=True):
+            english_name = st.text_input("English Name 英文名")
+            chinese_name = st.text_input("Chinese Name 中文名")
+
+            gender = st.radio("Gender 性别", ["Male 男", "Female 女"])
+            age = st.radio("Age 年龄", ["Prefer not to say 不愿说", "0-12", "13-18", "19-30", "31-59", "60+"])
+
+            phone_number = st.text_input("Phone Number 电话号码")
+            address = st.text_input("Address 地址")
+
+            language = st.radio("Language 语言", ["English 英语", "Mandarin 普通话", "Cantonese 粤语"])
+            purpose_of_visit = st.radio("I am 我是", ["Visitor 访客", "New Resident 新居民"])
+            faith_status = st.radio(
+                "I am 我是", ["Interested in becoming a Christian 有兴趣成为基督徒", "Christian 基督徒", "Other 非基督徒"]
+            )
+
+            add_member_button = st.form_submit_button("Add member 加成员")
+            if add_member_button:
+                if english_name or chinese_name:
+                    member_id = db.add_member([
+                        english_name, chinese_name, 
+                        gender, age, phone_number, address, 
+                        language, purpose_of_visit, 
+                        faith_status, st.session_state["family_id"]
+                    ])
+                    db.add_attendance(member_id)
+
+                    if not english_name:
+                        english_name = ""
+                    if not chinese_name:
+                        chinese_name = ""
+                    st.session_state["family"].append([english_name, chinese_name])
+
+                    st.success("Member added! 已加成员!")
+                    st.rerun()
+                else:
+                    st.warning("Name: Required 姓名：必填")
