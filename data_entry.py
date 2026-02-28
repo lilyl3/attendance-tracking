661c89345e77be307676bf6bdbe8977457ed6f09
lilyl3 <lily.lin.915@gmail.com>
2026-02-28 14:10:43 -0800

    Fix to create family before adding member

--- data_entry.py
+++ data_entry.py
@@ -16,14 +16,23 @@ if not csv_file.is_file():
 # Reads an excel file 
 data_df = pd.read_excel(
     csv_file,
-    names=["english_name", "chinese_name"]
+    names=["english_name", "chinese_name", "index", "family_name"]
 )
-
 # Strip white spaces at the ends of string
 data_df["english_name"] = data_df["english_name"].str.strip()
 data_df["chinese_name"] = data_df["chinese_name"].str.replace(r"\s+", "", regex=True)
 
+# Add families to database
+families = data_df[["index", "family_name"]].drop_duplicates()
+db = get_db()
+family_ids = []
+for _, row in families.iterrows():
+    family_ids.append(db.add_family(row.family_name))
+families["family_id"] = family_ids
+data_df = data_df.merge(families[["index", "family_id"]], on='index', how='inner')
+
 # Add members to database
 db = get_db()
-for row in data_df.itertuples(index=False):
-    db.add_member(row.english_name, row.chinese_name)
+for _, row in data_df.iterrows():
+    member_data = [row.english_name, row.chinese_name] + [""] * 7 + [row.family_id]
+    db.add_member(member_data)
