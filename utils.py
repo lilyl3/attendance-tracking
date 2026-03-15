c7b862fd6b8578f34c267d9f89ef8c11eff19922
lilyl3 <lily.lin.915@gmail.com>
2026-03-14 21:15:52 -0700

    Set up paths to store .csv for attendance reports

--- utils.py
+++ utils.py
@@ -4,13 +4,23 @@ from dateutil.relativedelta import relativedelta
 
 import os as os
 
-def attendance_file_path():
+def trend_paths(year):
+    os.makedirs("trend", exist_ok=True)
+    base_dir = Path("trend")
+    path = base_dir / f"{str(year)}.csv"
+    new_friends_path = base_dir / f"{str(year)} (new).csv"
+    return path, new_friends_path
+
+def attendance_file_paths(sunday_date):
     base_dir = Path("sunday_attendance")
-    month, year = datetime.now().strftime("%m"), datetime.now().strftime("%Y")
+    month, year = sunday_date.strftime("%m"), sunday_date.strftime("%Y")
     folder_path = base_dir / year / month
     os.makedirs(folder_path, exist_ok=True)
-    file_path = folder_path / (most_recent_sunday() + ".csv")
-    return file_path
+
+    sunday_date = format_date(sunday_date, iso=True)
+    file_path = folder_path / f"{sunday_date}.csv"
+    new_friends_file_path = folder_path / f"{sunday_date} (new).csv"
+    return file_path, new_friends_file_path
 
 # Iso formats date as YYYY-MM-DD
 def format_date(date_to_format, iso):
